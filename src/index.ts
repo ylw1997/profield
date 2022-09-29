cf0e3f4d7fbfe62721a88bf95ae9ab735863e61b
yangliwei <1280426581@qq.com>
2022-09-29 15:29:03 +0800

    去掉editor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: yangliwei 1280426581@qq.com
  * @Date: 2022-09-26 17:00:18
- * @LastEditTime: 2022-09-29 14:21:47
+ * @LastEditTime: 2022-09-29 15:28:05
  * @LastEditors: yangliwei 1280426581@qq.com
  * @FilePath: \vite-npm\src\index.ts
  * Copyright (c) 2022 by yangliwei 1280426581@qq.com, All Rights Reserved. 
@@ -13,14 +13,13 @@ import YUpload from "./components/YUpload";
 import proForm from "./components/proForm.vue";
 import proPanel from "./components/proPanel"
 import proTable from "./components/proTable"
-import proEditor from "./components/proEditor.vue"
 import { columnItem, ColumnsTypes, DefaultOptionType } from './types/index';
 
-export { proField,YUpload,proForm,proPanel,proTable,proEditor } //实现按需引入*
+export { proField,YUpload,proForm,proPanel,proTable } //实现按需引入*
 
 export type {ColumnsTypes,columnItem,DefaultOptionType}
 
-const components = [proField,YUpload,proForm,proPanel,proTable,proEditor];
+const components = [proField,YUpload,proForm,proPanel,proTable];
 
 const install = (app:App) => {
     components.forEach((component) => {
